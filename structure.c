4521b85805f4ca7d8b11680b60255fe94e3bfdc4
ArthurH54 <arthurhenriquesky22@gmail.com>
2026-05-22 11:58:22 -0300

    Add files via upload

--- structure.c
+++ structure.c
@@ -3,8 +3,21 @@
 #include <string.h>
 #include "structure.h"
 
+typedef struct _Node {
+    void* data;
+    struct _Node* prev;
+    struct _Node* next;
+} Node;
+
+//////////////////
+
+typedef struct {
+    Node* top;
+    size_t length;
+} _Stack;
+
 /**
- * @brief Initializes the stack, if it's not already initialized
+ * @brief Initializes the stack
  * @returns Stack
  */
 Stack stack_create() { 
@@ -24,24 +37,21 @@ void stack_push(Stack stack, const void* element, size_t size) {
     // Creates the node to be pushed and verifies if it's correctly initialized
     Node* newNode = (Node*) calloc(1, sizeof(Node));
     /* if error */ if(newNode == NULL) return;
+    newNode->data = malloc(size); 
+    /* if error */ if(newNode->data == NULL) { free(newNode); return; }                                 
+    memcpy(newNode->data, element, size);
 
     // If stack is empty, pushes the node as the top member
-    if(stack->top == NULL) {                                                
-        stack->top = newNode;                                               
-        stack->top->data = malloc(size); 
-        /* if error */ if(stack->top->data == NULL) { free(stack->top); return; }
-        memcpy(stack->top->data, element, size);                               
+    if(stack->top == NULL) {                                                                                                   
+        stack->top = newNode;                         
     }
 
     // If stack has elements, removes the previous node from the top and then pushes the new node
-    else {                                                                  
+    else {                                                                                                               
         stack->top->next = newNode;                                         
         stack->top->next->prev = stack->top;        
         stack->top = stack->top->next;                                      
-        stack->top->next = NULL;                                            
-        stack->top->data = malloc(size); 
-        /* if error */ if(stack->top->data == NULL) { free(stack->top); return; }                                 
-        memcpy(stack->top->data, element, size);                               
+        stack->top->next = NULL;                              
     }
 
     stack->length++;
@@ -59,6 +69,7 @@ void stack_pop(Stack stack) {
     if(stack->length == 1) {
         free(stack->top->data);
         free(stack->top);
+        stack->top = NULL;
         stack->length--;
     }
 
@@ -79,12 +90,12 @@ void stack_pop(Stack stack) {
  * @brief Removes all elements of the stack
  * @param stack The stack to be emptied
 */
-void stack_empty(Stack stack) {
+void stack_clear(Stack stack) {
     // Verifies if the stack isn't already empty
     if(stack == NULL || stack->top == NULL) return;
 
     // Pops stack until it's empty
-    while(stack->length != 0) popStack(stack);
+    while(stack->length != 0) stack_pop(stack);
 }
 
 /**
@@ -93,7 +104,7 @@ void stack_empty(Stack stack) {
  */
 void stack_destroy(Stack stack) { 
     if(stack != NULL) {
-        emptyStack(stack);
+        stack_clear(stack);
         free(stack); 
     }
 }
@@ -108,25 +119,6 @@ const void* stack_peek(const Stack stack) {
     else return stack->top->data;
 }
 
-/** 
- * @brief Gets a member of the stack, counting from the top
- * @param stack The stack whose member will be gotten
- * @param depth The depth of the member in the stack
- * @returns const void *
-*/
-// const void* stack_item(const Stack stack, size_t depth) {
-    if(stack == NULL) return NULL;
-
-    // Verifies if the given depth is larger than the length
-    if(depth >= stack->length) return NULL; 
-
-    // Iterates until it finds the member of given depth
-    Node* member = stack->top;
-    for(size_t i = 0; i < depth; i++) member = member->prev;
-
-    return member->data;
-}
-
 /**
  * @brief Returns the length of the stack
  * @param stack The stack whose size will be gotten
@@ -139,3 +131,138 @@ size_t stack_length(const Stack stack) {
 
 //////////////////
 
+typedef struct {
+    Node* head;
+    Node* tail;
+    size_t length;
+} _Queue;
+
+/**
+ * @brief Initializes the queue
+ * @returns Queue
+ */
+Queue queue_create() {
+    Queue queue = calloc(1, sizeof(_Queue));
+    return queue;
+}
+
+/**
+ * @brief Pushes a new member onto the tail of the queue
+ * @param stack Pointer to the queue
+ * @param element Pointer to the element to be copied
+ * @param size The size of the element in bytes
+ */
+void queue_enqueue(Queue queue, const void* element, size_t size) {
+if(queue == NULL || element == NULL || size == 0) return;                                               
+
+    // Creates the node to be pushed and verifies if it's correctly initialized
+    Node* newNode = (Node*) calloc(1, sizeof(Node));
+    /* if error */ if(newNode == NULL) return;
+
+    // If queue is empty, pushes the node as the head and tail member
+    if(queue->head == NULL) {                                                
+        queue->head = newNode;                                               
+        queue->head->data = malloc(size); 
+        /* if error */ if(queue->head->data == NULL) { free(queue->head); return; }
+        memcpy(queue->head->data, element, size); 
+        queue->tail = queue->head;                              
+    }
+
+    // If queue has elements, removes the previous node from the tail and then pushes the new node
+    else {                                                                  
+        queue->tail->next = newNode;                                         
+        queue->tail->next->prev = queue->tail;        
+        queue->tail = queue->tail->next;                                      
+        queue->tail->next = NULL;                                            
+        queue->tail->data = malloc(size); 
+        /* if error */ if(queue->tail->data == NULL) { free(queue->tail); return; }                                 
+        memcpy(queue->tail->data, element, size);                               
+    }
+
+    queue->length++;
+}
+
+/**
+ * @brief Removes the first element of the queue, if the queue is not empty
+ * @param queue Pointer to the queue
+ */
+void queue_dequeue(Queue queue) {
+    // Verifies if queue is uninitialized or empty
+    if(queue == NULL || queue->head == NULL) return;
+
+    // If it's the last element 
+    if(queue->length == 1) {
+        free(queue->head->data);
+        free(queue->head);
+        queue->head = NULL;
+        queue->tail = NULL;
+        queue->length--;
+    }
+
+    // Saves the reference to the next node, pops the head and assigns the previous node as the head
+    else {
+        Node* nextNode;                 
+        nextNode = queue->head->next;        
+        free(queue->head->data);
+        free(queue->head);
+        queue->head = nextNode;
+        queue->head->prev = NULL;
+
+        queue->length--;
+    }
+}
+
+/** 
+ * @brief Removes all elements of the queue
+ * @param queue The queue to be emptied
+*/
+void queue_clear(Queue queue) {
+    // Verifies if the queue isn't already empty
+    if(queue == NULL || queue->head == NULL) return;
+
+    // Pops stack until it's empty
+    while(queue->length != 0) queue_dequeue(queue);
+}
+
+/**
+ * @brief Destroys the queue, if it's not already destroyed
+ * @param queue The queue to be destroyed
+ */
+void queue_destroy(Queue queue) {
+    if(queue != NULL) {
+        queue_clear(queue);
+        free(queue); 
+    }
+}
+
+/**
+ * @brief Returns the content of the head of the queue
+ * @param queue The queue whose head will be gotten
+ * @returns const void *
+ */
+const void* queue_head(const Queue queue) {
+    if(queue == NULL || queue->head == NULL) return NULL;
+    else return queue->head->data;
+}
+
+/**
+ * @brief Returns the content of the tail of the queue
+ * @param queue The queue whose tail will be gotten
+ * @returns const void *
+ */
+const void* queue_tail(const Queue queue) {
+    if(queue == NULL || queue->tail == NULL) return NULL;
+    else return queue->tail->data;
+}
+
+/**
+ * @brief Returns the length of the queue
+ * @param queue The queue whose size will be gotten
+ * @returns int
+ */
+size_t queue_length(const Queue queue) {
+    if(queue == NULL) return 0; 
+    else return queue->length;
+}
+
+//////////////////
